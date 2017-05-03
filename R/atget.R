af72e64058a40d7217746123c70aa6b8f9afa226
liuyifei0226 <liuyifei0226@Gmail.com>
2017-05-03 16:33:59 -0400

    Add files via upload

---
+++ R/atget.R
@@ -0,0 +1,17 @@
+#' A Academic Torrent R download function
+#'
+#' This function allows you to download data through torrent.
+#' @param To be added in the future
+#' @keywords download
+#' @export
+#' @examples
+#' atget()
+
+atget <- function(name){
+       
+    script <- paste(system.file(package="atdownload"), "atdownload.py", sep="/")
+  	system2('python',args=c(as.character(script), as.character(name)))
+  	
+  	
+}
+
